573d1260d2417b8a9a1c56cc5c3bf71b0b570c08
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2023-11-01 15:04:46 +0900

    Feat : 칵테일 추천

--- src/main/kotlin/com/falco/cocktaildakk/repository/CocktailRepository.kt
+++ src/main/kotlin/com/falco/cocktaildakk/repository/CocktailRepository.kt
@@ -2,5 +2,15 @@ package com.falco.cocktaildakk.repository
 
 import com.falco.cocktaildakk.domain.cocktail.Cocktail
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface CocktailRepository : JpaRepository<Cocktail, Long>
+interface CocktailRepository : JpaRepository<Cocktail, Long> {
+
+    @Query(value = "SELECT * FROM cocktail ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    fun findRandomCocktails(@Param("limit") limit: Int): List<Cocktail>
+
+    fun findTop5ByKeywordsContains(keyword: String): List<Cocktail>
+    
+    fun findTop5ByBaseLiquorContaining(keyword: String): List<Cocktail>
+}
