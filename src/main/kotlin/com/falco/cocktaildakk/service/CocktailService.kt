13a2f2a1edfa3098704b9d60aa1a25da7866dd1f
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2023-11-02 18:06:00 +0900

    Feat : 칵테일 검색

--- src/main/kotlin/com/falco/cocktaildakk/service/CocktailService.kt
+++ src/main/kotlin/com/falco/cocktaildakk/service/CocktailService.kt
@@ -13,4 +13,17 @@ class CocktailService(
     fun getCocktailById(id: Long): Cocktail =
         cacheService.getCocktails().find { it.id == id } ?: throw BaseException(CommonErrorCode.NOT_EXIST_COCKTAIL)
 
+    fun queryCocktail(query: String): List<Cocktail> {
+        return cacheService.getCocktails().asSequence().filter {
+            // TODO 이게 맞나??
+            it.baseLiquor.contains(query) ||
+                    it.keywords.contains(query) ||
+                    it.description.contains(query) ||
+                    it.englishName.contains(query) ||
+                    it.koreanName.contains(query) ||
+                    it.ingredients.contains(query) ||
+                    it.mixingMethod.contains(query)
+        }.take(10).toList()
+    }
+
 }
